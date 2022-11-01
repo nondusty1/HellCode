a1fdb7c660278b118910de14fc9e3643f2917d90
Artem <artem.nodusty@gmail.com>
2022-11-01 19:39:13 +0300

    New edit Example012

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -89,35 +89,71 @@ string res = Method4(10, "qwer");
 // маленькие буквы "к" заменить большими буквами "К",
 // а большие "c" заменить маленькими "C".
 
-string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
-            + "ежели бы вас вместо нашего милого Винценгероде,"
-            + "вы бы взяли приступом согласие прусского короля."
-            + "Вы так красноречивы. Вы дадите мне чаю?";
-
+//string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+//            + "ежели бы вас вместо нашего милого Винценгероде,"
+//            + "вы бы взяли приступом согласие прусского короля."
+//            + "Вы так красноречивы. Вы дадите мне чаю?";
+//
 //string s = "qwert"
 //            01234
 // s[3] // r
+//
+//string Replace(string text, char oldValue, char newValue)
+//{
+//    string result = String.Empty;
+//
+//    int length = text.Length;
+//    for (int i = 0; i < length; i++)
+//    {
+//        if (text[i] == oldValue) result = result + $"{newValue}";
+//        else result = result + $"{text[i]}";
+//    }
+//
+//    return result;
+//}
+//
+//string newText = Replace(text, ' ', '|');
+//
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'с', 'С');
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'к', 'К');
+//Console.WriteLine(newText);
 
-string Replace(string text, char oldValue, char newValue)
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
+    int count = array.Length;
 
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < count; i++)
     {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]} ");
     }
-
-    return result;
+    Console.WriteLine();
 }
 
-string newText = Replace(text, ' ', '|');
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
 
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'с', 'С');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
